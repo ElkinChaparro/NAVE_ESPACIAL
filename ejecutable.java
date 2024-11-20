473a19e6a05b0cf5bb93774419a6d3b6b9d24bbf
Elkin Humberto Chaparro Franklin <elkinchaparro@gmail.com>
2024-11-19 22:19:13 -0500

    Update

--- ejecutable.java
+++ ejecutable.java
@@ -1,3 +1,45 @@
 public class ejecutable {
+    public static void main(String[] args) {
+        System.out.println("|=======================================|");
+        System.out.println("|       Viajes Interplanetarios         |");
+        System.out.println("|=======================================|");
+        var option = 0;
+        switch (option) {
+            case 1:
+                switch (args) {
+                    case value:
 
+                        break;
+
+                    default:
+                        break;
+                }
+                break;
+
+            case 2:
+                switch (args) {
+                    case value:
+                        for (int i = 0; i < args.length; i++) {
+
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+                break;
+            case 3:
+if (&&) {
+    
+}
+                break;
+            case 4:
+System.out.println();
+                break;
+
+            default:
+                break;
+        }
+
+    }
 }
